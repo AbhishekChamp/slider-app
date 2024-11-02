6319b8008cb618f10fbc186c39d6e53cd65d85c6
coderdeck <contactcoderdeck@gmail.com>
2024-11-02 20:08:51 +0530

    added js content

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-console.log("Hello");
+let currentSlide = 0;
 
-function showSlide(index) {}
+function showSlide(index) {
+    const slides = document.querySelectorAll(".slide");
+    const totalSlides = slides.length;
 
-function moveSlide(direction) {}
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    const slideWidth = -currentSlide * 100;
+    document.querySelector(
+        ".slides",
+    ).style.transform = `translateX(${slideWidth}%)`;
+}
+
+function moveSlide(direction) {
+    showSlide(currentSlide + direction);
+}
+
+// Initialize the slider
+showSlide(currentSlide);
